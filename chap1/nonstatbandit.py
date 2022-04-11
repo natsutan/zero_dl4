bd762f9dc4e784bfed29a0d69de3f865c5cd2e89
natsutan <YRW03704@nifty.com>
2022-04-11 19:40:13 +0900

    1章終了

--- chap1/nonstatbandit.py
+++ chap1/nonstatbandit.py
@@ -6,6 +6,7 @@ steps = 1000
 epsilon = 0.1
 all_rates = np.zeros((runs, steps))
 
+
 class Bandit:
     def __init__(self, arms=10):
         self.rates = np.random.rand(arms)
@@ -16,6 +17,21 @@ class Bandit:
             return 1
         else:
             return 0
+
+
+class NonStatBandit:
+    def __init__(self, arms=10):
+        self.rates = np.random.rand(arms)
+        self.arms = arms
+
+    def play(self, arm):
+        rate = self.rates[arm]
+        self.rates += 0.1 * np.random.rand(self.arms)
+        if rate > np.random.rand():
+            return 1
+        else:
+            return 0
+
 
 class Agent:
     def __init__(self, epsilon, action_size=10):
@@ -33,10 +49,26 @@ class Agent:
         return np.argmax(self.Qs)
 
 
+class AlphaAgent:
+    def __init__(self, epsilon, alpha, action_size=10):
+        self.epsilon = epsilon
+        self.Qs = np.zeros(action_size)
+        self.alpha = alpha
+
+    def update(self, action, reward):
+        self.Qs[action] += (reward - self.Qs[action]) * self.alpha
+
+    def get_action(self):
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(0, len(self.Qs))
+        return np.argmax(self.Qs)
+
+
 def main():
     for run in range(runs):
-        bandit = Bandit()
-        agent = Agent(epsilon)
+        bandit = NonStatBandit()
+        #agent = Agent(epsilon)
+        agent = AlphaAgent(epsilon, 0.8)
         total_reward = 0
         rates = []
 
@@ -55,6 +87,7 @@ def main():
 
     plt.ylabel('Rates')
     plt.xlabel('Steps')
+    plt.ylim(0.8, 1.0)
     plt.plot(avg_rates)
     plt.show()
 
