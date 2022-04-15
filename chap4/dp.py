a1350d617dff8f40767d0ad619d402350c741298
natsutan <YRW03704@nifty.com>
2022-04-15 22:27:37 +0900

    4.2まで

--- chap4/dp.py
+++ chap4/dp.py
@@ -1,21 +1,55 @@
 import sys
+from collections import defaultdict
+import numpy as np
 
 sys.path.append("../common")
 from World import GridWorld
 
 
+def eval_onestep(pi, V, env, gamma=0.9):
+    for state in env.states():
+        if state == env.goal_state:
+            V[state] = 0
+            continue
+
+        acton_probs = pi[state]
+        new_V = 0
+
+        for action, acton_probs in acton_probs.items():
+            next_state = env.next_state(state, action)
+            r = env.reward(state, action, next_state)
+            new_V += acton_probs * (r + gamma * V[next_state])
+
+        V[state] = new_V
+
+    return V
+
+def policy_eval(pi, V, env, gamma, threshold = 0.001):
+    while True:
+        old_V = V.copy()
+        V = eval_onestep(pi, V, env, gamma)
+
+        delta = 0
+        for state in V.keys():
+            t = abs(V[state] - old_V[state])
+            if delta < t:
+                delta = t
+
+        if delta  < threshold:
+            break
+
+    return V
+
+
 def main():
+    gamma = 0.9
     env = GridWorld()
-    print(env.height)
-    print(env.width)
-    print(env.shape)
+    V = defaultdict(lambda: 0)
+    pi = defaultdict(lambda: {0: 0.25, 1:0.25, 2:0.25, 3:0.25})
 
-    for a in env.actions() :
-        print(a)
+    V = policy_eval(pi, V, env, gamma)
 
-    print("===")
-    for s in env.states():
-        print(s)
+    env.render_v(V, pi)
 
 
 if __name__ == '__main__':
--- common/World.py
+++ common/World.py
@@ -1,4 +1,5 @@
 import numpy as np
+import common.gridworld_render as render_helper
 
 class GridWorld:
     def __init__(self):
@@ -41,6 +42,41 @@ class GridWorld:
             for w in range(self.width):
                 yield (h, w)
 
-
     def next_state(self, state, action):
-        ac
+        action_move_map = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        move = action_move_map[action]
+        next_state = (state[0] + move[0], state[1] + move[1])
+        ny, nx = next_state
+
+        if nx < 0 or nx >= self.width or ny < 0 or ny >= self.height:
+            next_state = state
+        elif next_state == self.wall_state:
+            next_state = state
+
+        return next_state
+
+    def reward(self, state, action, next_state):
+        return self.reward_map[next_state]
+
+    def reset(self):
+        self.agent_state = self.start_state
+        return self.agent_state
+
+    def step(self, action):
+        state = self.agent_state
+        next_state = self.next_state(state, action)
+        reward = self.reward(state, action, next_state)
+        done = (next_state == self.goal_state)
+
+        self.agent_state = next_state
+        return next_state, reward, done
+
+    def render_v(self, v=None, policy=None, print_value=True):
+        renderer = render_helper.Renderer(self.reward_map, self.goal_state,
+                                          self.wall_state)
+        renderer.render_v(v, policy, print_value)
+
+    def render_q(self, q=None, print_value=True):
+        renderer = render_helper.Renderer(self.reward_map, self.goal_state,
+                                          self.wall_state)
+        renderer.render_q(q, print_value)
