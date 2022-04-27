b423099d6f468f06d85fa573b5afcbba29b52e99
natsutan <YRW03704@nifty.com>
2022-04-27 16:23:11 +0900

    Chap7 skip

---
+++ chap7/nn.py
@@ -0,0 +1,14 @@
+import numpy as np
+from dezero import Variable
+import dezero.functions as F
+
+np.random.seed(0)
+x = np.random.rand(100, 1)
+y = np.sin(2 * np.pi * x) + np.random.rand(100, 1)
+
+I, H, O = 1, 10, 1
+W1 = Variable(0.01 * np.random.randn(I, H))
+b1 = Variable(np.zeros(H))
+W2 = Variable(0.01 * np.random.randn(H, O))
+b2 = Variable(np.zeros(O))
+
